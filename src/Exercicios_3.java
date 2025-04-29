ef2e8161d353131ba347a1d95ddd6608decd16aa
tarcisio_souto <tss.labsi@gmail.com>
2025-04-29 13:16:55 -0300

    praticando com exercicios sobre vetores

--- src/Exercicios_3.java
+++ src/Exercicios_3.java
@@ -1,4 +1,7 @@
+import javafx.scene.control.RadioMenuItem;
+
 import javax.swing.*;
+import java.util.Random;
 
 public class Exercicios_3 {
 
@@ -8,7 +11,9 @@ public class Exercicios_3 {
     {
         //ArrayNumbers();
         //ArrayImprimePares();
-        ArrayDobro();
+        //ArrayDobro();
+        //System.out.println(ArrayMaiorMenor());
+        SomaVetores();
     }
 
     /* Crie um programa que leia 5 números inteiros e armazene em um array. Depois, mostre todos os números digitados. */
@@ -78,5 +83,99 @@ public class Exercicios_3 {
         }
 
     }
+
+    /* Preencha um vetor de 6 posições com números inteiros aleatórios entre 1 e 100 e depois exiba o maior e o menor valor armazenado. */
+
+    public static String ArrayMaiorMenor()
+    {
+        int[] numbers = new int[6];
+        Random rand = new Random();
+        int numMax;
+        int numMin;
+
+        for (int i = 0; i < numbers.length; i++)
+        {
+            numbers[i] = rand.nextInt(100);
+        }
+
+        /* Verificar quais números são maior e menor */
+        numMin = numbers[0];
+        numMax = numbers[0];
+
+        for (int i : numbers)
+        {
+            if (numMin < i)
+                numMin = i;
+            if (numMax > i)
+                numMax = i;
+        }
+
+        return "" + "\n" +  "Min: " + numMin + " | Max: " + numMax;
+
+    }
+
+    /* Leia dois vetores de 5 posições cada e crie um terceiro vetor que armazene a soma dos elementos dos dois primeiros vetores, posição por posição. */
+
+    public static void SomaVetores()
+    {
+        int[] arr1 = new int[5];
+        int[] arr2 = new int[5];
+        int[] arrSoma = new int[5];
+
+        Random rand = new Random();
+
+        /* atribuindo os valores aos vetores */
+        for (int i = 0; i < arr1.length; i++)
+        {
+            arr1[i] = rand.nextInt(20);
+            arr2[i] = rand.nextInt(20);
+            arrSoma[i] = arr1[i] + arr2[i];
+        }
+
+        /* exibindo o vetor 1*/
+        for (int i = 0; i < arrSoma.length; i++)
+        {
+            System.out.println("Vetor 1: " + arr1[i]);
+        }
+        /* exibindo o vetor 2*/
+        for (int i = 0; i < arrSoma.length; i++)
+        {
+            System.out.println("Vetor 2: " + arr2[i]);
+        }
+        /* exibindo o vetor soma*/
+        for (int i = 0; i < arrSoma.length; i++)
+        {
+            System.out.println("Vetor Soma: " + arrSoma[i]);
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
