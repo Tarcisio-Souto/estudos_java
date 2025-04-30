bccdfffc8e7fecaea1aa2e0621b137c751954a14
tarcisio_souto <tss.labsi@gmail.com>
2025-04-30 12:53:03 -0300

    praticando mais um pouco com matrizes

--- src/Exercicios_4.java
+++ src/Exercicios_4.java
@@ -8,7 +8,8 @@ public class Exercicios_4 {
     {
         //MatrizExemplo1();
         //MatrizExemplo2();
-        MatrizExemplo3();
+        //MatrizExemplo3();
+        MatrizExemplo4();
     }
 
     /* Crie uma matriz 3x3 e preencha com valores informados pelo usuário. Em seguida, exiba a matriz. */
@@ -98,5 +99,45 @@ public class Exercicios_4 {
         }
 
     }
+
+    /* Preencha uma matriz 6x6 com 0 na diagonal principal e 1 nos demais elementos. */
+
+    public static void MatrizExemplo4()
+    {
+        int[][] mtz = new int[6][6];
+
+        for (int i = 0; i < mtz.length; i++) {
+            for (int j = 0; j < mtz.length; j++) {
+                if (j == i)
+                    mtz[i][j] = 0;
+                else
+                    mtz[i][j] = 1;
+
+            }
+        }
+
+        for (int i = 0; i < mtz.length; i++)
+        {
+            for (int j = 0; j < mtz.length; j++)
+            {
+                System.out.print(mtz[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
