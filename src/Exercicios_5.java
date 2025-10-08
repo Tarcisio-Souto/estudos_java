97cb2989994705a261e518f24d15e137234cdc90
tarcisio_souto <tss.labsi@gmail.com>
2025-10-07 23:03:49 -0300

    retomando os estudos com java com uma revisao

---
+++ src/Exercicios_5.java
@@ -0,0 +1,24 @@
+public class Exercicios_5 {
+
+    /* EXERCÍCIOS SOBRE Orientação a objetos (classes, objetos, herança, polimorfismo) */
+
+    /*
+        Crie uma classe chamada Livro que possua os seguintes atributos: titulo, autor e anoPublicacao.
+        Crie um método para exibir as informações do livro. Em seguida, crie dois objetos dessa classe e chame o método para exibir os dados.
+     */
+
+    public static void Livro()
+    {
+        String titulo;
+        String autor;
+        Integer anoPublicacao;
+
+        
+
+
+    }
+
+
+
+
+}
---
+++ src/Exercicios_6_revisao.java
@@ -0,0 +1,50 @@
+import javax.swing.*;
+import java.lang.reflect.Array;
+
+
+public class Exercicios_6_revisao
+{
+    public static void main(String args[])
+    {
+        /* resultado da soma dos números: */
+        //JOptionPane.showMessageDialog(null, "O resultado da soma é: " + SomarNumeros());
+
+
+        /* resultado da lista de nomes: */
+        JOptionPane.showMessageDialog(null, PegaNomes());
+
+    }
+
+
+    /* somar dois números informados pelo usuário */
+
+    public static Integer SomarNumeros()
+    {
+        int num1 = 0;
+        int num2 = 0;
+
+        num1 = Integer.parseInt(JOptionPane.showInputDialog("Insira o primeiro número: "));
+        num2 = Integer.parseInt(JOptionPane.showInputDialog("Insira o segundo número: "));
+
+        return (num1 + num2);
+
+    }
+
+    /* ler 3 nomes, armazenar em um array e exibí-los */
+
+    public static String[] PegaNomes()
+    {
+        String[] nomes = new String[3];
+
+        for (int i=0; i<3; i++)
+        {
+            String nome = JOptionPane.showInputDialog("Informe o " + (i+1) + "º nome: ");
+            nomes[i] = nome;
+        }
+
+        return nomes;
+
+    }
+
+
+}
